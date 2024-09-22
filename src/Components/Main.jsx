ca0a9650b8cc4d13f368bcf77dc763f72c83c107
SabaShagidze59 <saba.webdev59@gmail.com>
2024-09-22 21:50:42 +0400

    Initial commit

---
+++ src/Components/Main.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import "../Components/Main.css";
+
+const Main = () => {
+  return (
+    <div className="main">
+      <div className="main-wrapper">
+        <div className="main-wrapper-left">
+          <img src="../src/assets/geralt.png" alt="" />
+        </div>
+        <div className="main-wrapper-right">
+          <div className="main-card">
+            <div className="card-header">
+              <p>The Witcher 3</p>
+            </div>
+            <div className="card-text">
+              <p>
+                The Witcher 3: Wild Hunt is a 2015 action role-playing game
+                developed and published by CD Projekt. It is the sequel to the
+                2011 game The Witcher 2: Assassins of Kings and the third game
+                in The Witcher video game series, played in an open world with a
+                third-person perspective. The games follow the Witcher series of
+                fantasy novels written by Andrzej Sapkowski.
+              </p>
+            </div>
+            <div className="card-button">
+              <button>Pre-Order Now!</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { useRef } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import "../Components/Navbar.css";
+
+const Navbar = () => {
+  const navRef = useRef();
+
+  const showNavbar = () => {
+    navRef.current.classList.toggle("responsive_nav");
+  };
+
+  return (
+    <div className="header">
+      <h3>Logo</h3>
+      <nav ref={navRef}>
+        <a href="#">Home</a>
+        <a href="#">Shop</a>
+        <a href="#">About</a>
+        <a href="#">Contact</a>
+        <button className="nav-btn nav-close-btn" onClick={showNavbar}>
+          <FaTimes />
+        </button>
+      </nav>
+      <button className="nav-btn" onClick={showNavbar}>
+        <FaBars />
+      </button>
+    </div>
+  );
+};
+
+export default Navbar;
