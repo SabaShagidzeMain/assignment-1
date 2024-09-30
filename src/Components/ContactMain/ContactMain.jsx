f46c5d976185708d644ad95fa600f31dfb87b4ca
SabaShagidze2 <154365489+SabaShagidze2@users.noreply.github.com>
2024-10-01 00:26:02 +0400

    Finalized About And Contact Pages (Might change design later)

--- src/Components/ContactMain/ContactMain.jsx
+++ src/Components/ContactMain/ContactMain.jsx
@@ -2,9 +2,53 @@ import React from "react";
 import "./ContactMain.css";
 
 const ContactMain = () => {
-  return <main className="main">
-    <p>Halo</p>
-  </main>;
+  return (
+    <main className="main">
+      <div className="contactWrapper">
+        <form action="#">
+          <div className="input-container">
+            <label htmlFor="fname">First Name</label>
+            <input
+              type="text"
+              id="fname"
+              name="firstname"
+              placeholder="Your name..."
+            />
+          </div>
+          <div className="input-container">
+            <label htmlFor="lname">Last Name</label>
+            <input
+              type="text"
+              id="lname"
+              name="lastname"
+              placeholder="Your last name..."
+            />
+          </div>
+          <div className="input-container">
+            <label htmlFor="lname">Subject</label>
+            <input
+              type="text"
+              id="lname"
+              name="lastname"
+              placeholder="Your last name..."
+            />
+          </div>
+
+          <div className="input-container">
+            <label htmlFor="text">Write Here</label>
+            <textarea
+              type="text"
+              id="textAr"
+              name="subject"
+              placeholder="..."
+            />
+          </div>
+
+          <input type="submit" value="submit" />
+        </form>
+      </div>
+    </main>
+  );
 };
 
 export default ContactMain;
