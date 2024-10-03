3a55e822b68e9943c708229aa3af889f4e930d8c
SabaShagidze2 <154365489+SabaShagidze2@users.noreply.github.com>
2024-10-04 00:50:00 +0400

    Added Profile Link To NavBar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -15,11 +15,30 @@ const Navbar = () => {
     <header className="header">
       <h3>Logo</h3>
       <nav ref={navRef}>
-        <Link to="/">Home</Link>
-        <Link to="/About">About</Link>
-        <Link to="/Contact">Contact</Link>
-        <Link to="/BlogPage">Blog</Link>
-        <Link to="/Assignment3">Assignment 3</Link>
+        <div className="link-wrapper">
+          <Link to="/" className="nav-link">
+            Home
+          </Link>
+          <Link to="/About" className="nav-link">
+            About
+          </Link>
+          <Link to="/Contact" className="nav-link">
+            Contact
+          </Link>
+          <Link to="/BlogPage" className="nav-link">
+            Blog
+          </Link>
+          <Link to="/Assignment3" className="nav-link">
+            Assignment 3
+          </Link>
+          <Link to="">
+            <img
+              src="src/assets/user-images/serious-man-thinking_1149-1328.avif"
+              alt=""
+              className="profile-image"
+            />
+          </Link>
+        </div>
         <button className="nav-btn nav-close-btn" onClick={showNavbar}>
           <FaTimes />
         </button>
